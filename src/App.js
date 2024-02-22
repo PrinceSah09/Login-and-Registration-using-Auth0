951565bc9619e014bfb28cb8664edd5a3cde70d1
Prince Saha <pbaba7001@gmail.com>
2024-02-22 18:14:12 +0530

    [Feat] added heading-text and styles

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import { useAuth0 } from "@auth0/auth0-react";
+// Import necessary dependencies and components
+import { useAuth0 } from "@auth0/auth0-react"; // Importing the useAuth0 hook from the Auth0 library
 import Card from "./components/Card";
 import "./App.css";
 import "./components/Card.css";
+
+// CSS import for external fonts
 <style>
   @import
   url('https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&display=swap')
 </style>;
 
+// Define the main App component
 function App() {
+  // Destructuring properties from the useAuth0 hook
   const { user, loginWithRedirect, isAuthenticated, logout } = useAuth0();
 
+  // Return the JSX content of the App component
   return (
     <div className="content">
       <div className="App">
-        {isAuthenticated ? <Card data={user} /> : <p></p>}
+        <div className="headerText">
+          <h1>
+            Authentication System Using <br />{" "}
+            <span> Auth0 and Microsoft Identity</span>
+          </h1>
+        </div>
+        {isAuthenticated ? <Card data={user} /> : <p></p>}{" "}
+        {/* Conditional rendering of Card component based on user authentication */}
         {isAuthenticated ? (
           <button onClick={(e) => logout()}>Logout</button>
         ) : (
@@ -22,7 +35,7 @@ function App() {
               loginWithRedirect();
             }}
           >
-            Login with Redirect
+            Login or Sign-Up
           </button>
         )}
       </div>
@@ -30,4 +43,4 @@ function App() {
   );
 }
 
-export default App;
+export default App; // Export the App component as default
